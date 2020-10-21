32a578ce81ae4f079af03b6c32f46dbee6f07e9b
JR Mishra <jrmnitdgp@gmail.com>
2020-10-21 19:27:47 +0530

    [JR Mishra]Add. LinkedList Implimented

---
+++ DataStructuresImplimentation/LinkedList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructuresImplimentation
+{
+    class LinkedList<T>
+    {
+        internal Node<T> head;
+        int Length { get; set; } = 0;
+
+        internal void Add(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            if (this.head == null)
+                this.head = node;
+            else
+            {
+                Node<T> temp = head;
+                while (temp.next != null)
+                    temp = temp.next;
+                temp.next = node;
+                Console.WriteLine($"{node.data} inserted into Linked List");
+            }
+            Length++;
+        }
+
+        
+    }
+}
