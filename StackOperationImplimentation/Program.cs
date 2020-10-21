a07a55fbb283611683d8d8ac4cad5ec55b744a92
JR Mishra <jrmnitdgp@gmail.com>
2020-10-21 18:17:44 +0530

    [JR Mishra]Add. Project Initialized

---
+++ StackOperationImplimentation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StackOperationImplimentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Stack Operation Implimentation Program");
+            Console.WriteLine("==================================================");
+        }
+    }
+}
